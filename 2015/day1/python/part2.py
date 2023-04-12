73fc131d1e88b38a25f0157f6e27fae12cd08637
Daniel Beaver <daniel.beaver1012@gmail.com>
2023-04-12 10:40:10 -0400

    add part 2

--- 2015/day1/python/part2.py
+++ 2015/day1/python/part2.py
@@ -1,12 +1,14 @@
-# with open('data.txt') as raw:
-#     data = raw.read()
+with open('data.txt') as raw:
+    data = raw.read()
 
-# floor = 0
-
-# for char in data:
-#     if (char == "("):
-#         floor += 1
-#     else:
-#         floor -= 1
-
-# print(floor)
+floor = 0
+counter = 0
+for char in data:
+    counter += 1
+    if (char == "("):
+        floor += 1
+    else:
+        floor -= 1
+    if (floor < 0):
+        print(counter)
+        break
