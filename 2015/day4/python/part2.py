e714cb099a4338959c32f573a28cfa744d78450a
Daniel Beaver <danielbeaver@2023-DanielBeaver-R1.local>
2023-06-29 15:10:21 -0400

    add part 2

---
+++ 2015/day4/python/part2.py
@@ -0,0 +1,12 @@
+import hashlib
+
+suffix = 0
+code = 'bgvyzdsv'
+# result = hashlib.md5(b"Hello MD5").hexdigest()
+hash = ""
+
+while hash[0:6] != "000000":
+    suffix +=1
+    hash = hashlib.md5((code + str(suffix)).encode('utf-8')).hexdigest()
+
+print(suffix)
